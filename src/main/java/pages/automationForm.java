bc65c9ebef88762813439caa58089b233a0c7e91
Pranav <pranavdave1992@gmail.com>
2020-01-23 15:04:27 -0500

    Test base change to include util

--- src/main/java/pages/automationForm.java
+++ src/main/java/pages/automationForm.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 
 import tests.testBase;
 
 public class automationForm extends testBase{
 	
+	@SuppressWarnings("unused")
 	private WebDriver driver;
 	
 	 public automationForm(WebDriver crmDriver) {
--- src/test/java/tests/automationFormTest.java
+++ src/test/java/tests/automationFormTest.java
@@ -1,23 +1,18 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 
 import pages.automationForm;
-import utils.testUtils;
 
 public class automationFormTest extends testBase{
-	
-	testUtils formTest;
+
 	automationForm pageElements;
 	
 	@BeforeTest
 	public void before(){
 		pageElements = new automationForm(super.getDriver());
 		pageElements = PageFactory.initElements(crmDriver, automationForm.class);
-		
-		formTest = new testUtils(super.getDriver());
 	}
 	
 	@Test()
