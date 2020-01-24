ca1248cc4e152d9136ba1a667a70eba3dc1668f2
Pranav <pranavdave1992@gmail.com>
2020-01-24 15:18:33 -0500

    Aligning the structure

--- src/main/java/utils/testUtils.java
+++ src/main/java/utils/testUtils.java
@@ -10,26 +10,23 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-import tests.testBase;
+public class testUtils{
 
-public class testUtils extends testBase{
-	
 	private WebDriver driver;
-	
+
 	public testUtils(WebDriver crmDriver) {
-	       this.driver = crmDriver;
-	    }
-	
+		this.driver = crmDriver;
+	}
+
 	public void takeSnapShot() throws Exception{
-		
 		System.out.println(this.driver);
 		File SrcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		File DestFile=new File("failedTestCaseScreens/"+timestamp()+".png");
 		FileUtils.copyFile(SrcFile, DestFile);
 	}
-	
+
 	public static String timestamp() {
-        return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
-    }
-	
+		return new SimpleDateFormat("yyyy-MM-dd HH-mm-ss").format(new Date());
+	}
+
 }
--- src/test/java/tests/automationFormTest.java
+++ src/test/java/tests/automationFormTest.java
@@ -1,24 +1,22 @@
 package tests;
 
-import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.*;
 
 import pages.automationForm;
 
 public class automationFormTest extends testBase{
 
-	automationForm pageElements;
-	
+	public automationForm pageElements;
+
 	@BeforeTest
 	public void before(){
-		pageElements = new automationForm(super.getDriver());
-		pageElements = PageFactory.initElements(crmDriver, automationForm.class);
+		pageElements = new automationForm(getDriver());
 	}
-	
+
 	@Test()
-	public void test1() throws Exception{
+	public void fillingTheForm() throws Exception{
 		pageElements.enterName("Pranav","Dave");
 		util.takeSnapShot();
 	}
-	
+
 }
